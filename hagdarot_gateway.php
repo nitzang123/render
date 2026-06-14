669eaf7b64b0660aca07ee3a3297a81d96055be1
nitzang123 <70280184+nitzang123@users.noreply.github.com>
2026-06-14 19:26:40 +0300

    Update hagdarot_gateway.php

--- hagdarot_gateway.php
+++ hagdarot_gateway.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'gateway_token' => 'K3SEfC7j3NdWfQXRzP5hSGbUUDLtXXVTj5Tn',
-    'main_server_token' => 'G%%$o4uDVp9%f9bh9$1g_!'
+    'gateway_token' => '',
+    'main_server_token' => ''
 ];
 ?>
